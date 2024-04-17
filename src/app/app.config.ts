fcc6f35dfcb975e686aa30155a92bd4335d4303c
rcski77 <rcski77@gmail.com>
2024-04-17 13:11:40 -0400

    update ignore file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { firebaseAPI } from '../environments/environment'
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"rykse-cis655-project","appId":"1:768682786425:web:c12f885d0ba1450b5d9ea8","storageBucket":"rykse-cis655-project.appspot.com","apiKey":"AIzaSyAy2WpyYE-PI0qL2QybT11PTQOHewWjhys","authDomain":"rykse-cis655-project.firebaseapp.com","messagingSenderId":"768682786425"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"rykse-cis655-project","appId":"1:768682786425:web:c12f885d0ba1450b5d9ea8","storageBucket":"rykse-cis655-project.appspot.com","apiKey":`${firebaseAPI.apiKey}`,"authDomain":"rykse-cis655-project.firebaseapp.com","messagingSenderId":"768682786425"}))), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
